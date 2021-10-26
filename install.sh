0a36bacd9c668c41b6a844e989187955fa368a74
alexandrenog <alexandre.nog212@gmail.com>
2021-10-26 19:30:53 -0300

    Script Instalador pro Linux

---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo apt-get install ruby-full build-essential libsdl2-dev libsdl2-ttf-dev libpango1.0-dev libgl1-mesa-dev libopenal-dev libsndfile-dev libmpg123-dev libgmp-dev libsqlite3-dev
+sudo gem install gosu sqlite3-ruby
